a85f659e97534d6599f70a6945e4b3f17bda33b0
vasilakhan0805 <azizzkhan1997@gmail.com>
2023-03-10 15:28:00 +0500

    clothes finished

--- src/clothes/index.jsx
+++ src/clothes/index.jsx
@@ -1,55 +1,54 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import { Container,Wrapper,InnerWrapper } from "./style";
+import { Container, Wrapper, InnerWrapper } from "./style";
 
+class Clothes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
 
-class Clothes extends Component{
-        constructor(props){
-            super(props);
-            this.state = {
-                products : [],
-            }
-        }
+  componentDidMount() {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => this.setState({ products: json }));
+  }
 
-        componentDidMount(){
-            fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=> this.setState({products: json}))
-        }
-
-
-    render(){
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 1200,
-            slidesToShow: 1,
-            slidesToScroll: 3,
-            arrows: true,
-            autoplay:true,
-            autoplaySpeed: 2000,
-            pauseOnFocus:true,
-        };
-            console.log(this.state);
-        return(
-            <Container>
-                <Wrapper>
-        <Slider {...settings}>
-        {this.state.products?.map((product) => (
-            <InnerWrapper>
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnFocus: true,
+      fade: true,
+      cssEase: "linear",
+    };
+    console.log(this.state);
+    return (
+      <Container>
+        <Wrapper>
+          <Slider {...settings}>
+            {this.state.products?.map((product) => (
+              <InnerWrapper>
                 <h3> {product.category.toUpperCase()}</h3>
                 <img src={product.image} alt="product.title" />
                 <InnerWrapper.P>{product.title}</InnerWrapper.P>
                 <InnerWrapper.P>${product.price}</InnerWrapper.P>
                 <InnerWrapper.P>{product.category}</InnerWrapper.P>
-            </InnerWrapper>
-          ))}
-        </Slider> 
-                </Wrapper>
-            </Container>
-        )
-    }
+              </InnerWrapper>
+            ))}
+          </Slider>
+        </Wrapper>
+      </Container>
+    );
+  }
 }
 
-
-export default Clothes
+export default Clothes;
